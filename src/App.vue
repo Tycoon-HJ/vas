4f4929be46445cc18a34d34e54eee21dd6c8bdc4
yinhaijun <2523284817@qq.com>
2024-10-14 21:31:54 +0800

    add ring chat

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 
     <div class="row">
       <div class="cell">
-        <dv-border-box-8 :reverse="true"></dv-border-box-8>
+        <dv-border-box-8 :reverse="true">
+          <RingChat></RingChat>
+        </dv-border-box-8>
       </div>
       <div class="cell">
         <dv-border-box-8 :reverse="true"></dv-border-box-8>
@@ -31,7 +33,13 @@
 </template>
 
 <script>
-export default {}
+import RingChat from "@/components/RingChat.vue";
+
+export default {
+  components: {
+    RingChat
+  }
+}
 </script>
 <style>
 body {
