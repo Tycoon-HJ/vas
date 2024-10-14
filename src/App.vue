a9c4927d6805d2981b6f97daac3a19f79a1154f7
yinhaijun <2523284817@qq.com>
2024-10-14 22:04:43 +0800

    add CapsuleChat.vue and FlylineChat.vue and ScrollBoard.vue

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,26 @@
     <div class="row">
       <div class="cell">
         <dv-border-box-8 :reverse="true">
-          <RingChat></RingChat>
+          <RingChat/>
         </dv-border-box-8>
       </div>
       <div class="cell">
-        <dv-border-box-8 :reverse="true"></dv-border-box-8>
+        <dv-border-box-8 :reverse="true">
+          <CapsuleChat/>
+        </dv-border-box-8>
       </div>
       <div class="cell">
-        <dv-border-box-8 :reverse="true"></dv-border-box-8>
+        <dv-border-box-8 :reverse="true">
+          <FlylineChat/>
+        </dv-border-box-8>
       </div>
 
     </div>
     <div class="row">
       <div class="cell">
-        <dv-border-box-8 :reverse="true"></dv-border-box-8>
+        <dv-border-box-8 :reverse="true">
+          <ScrollBoard/>
+        </dv-border-box-8>
       </div>
       <div class="cell">
         <dv-border-box-8 :reverse="true"></dv-border-box-8>
@@ -34,10 +40,16 @@
 
 <script>
 import RingChat from "@/components/RingChat.vue";
+import CapsuleChat from "@/components/CapsuleChat.vue";
+import FlylineChat from "@/components/FlylineChat.vue";
+import ScrollBoard from "@/components/ScrollBoard.vue";
 
 export default {
   components: {
-    RingChat
+    RingChat,
+    CapsuleChat,
+    FlylineChat,
+    ScrollBoard
   }
 }
 </script>
---
+++ src/components/ScrollBoard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="box">
+    <dv-scroll-board :config="config" style="width:100%;height:80%" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      config: {
+        header: ['债券代码', '债券简称', '债券金额'],
+        data: [
+          ['<span style="color:#37a2da;">462663674</span>', '打打债券', '0.6'],
+          ['76266367', '<span style="color:#32c5e9;">国新债券</span>', '3.5'],
+          ['573784367', '行3列2', '<span style="color:#67e0e3;">45353</span>'],
+          ['256734678', '<span style="color:#9fe6b8;">离新债券</span>', '34234234.787'],
+          ['<span style="color:#ffdb5c;">257843673</span>', '行5列2', '423423'],
+          ['843367738', '<span style="color:#ff9f7f;">睁大债券</span>', '42345.99'],
+          ['114242563', '行7列2', '<span style="color:#fb7293;">45345</span>'],
+          ['684432556', '<span style="color:#e062ae;">姥姥债券</span>', '45345.99'],
+          ['<span style="color:#e690d1;">968699969</span>', '行9列2', '23424.90'],
+          ['523525668', '<span style="color:#e7bcf3;">呵呵债券</span>', '55.8678'],
+          ['843367738', '<span style="color:#ff9f7f;">睁大债券</span>', '42345.99'],
+          ['114242563', '行7列2', '<span style="color:#fb7293;">45345</span>'],
+          ['684432556', '<span style="color:#e062ae;">姥姥债券</span>', '45345.99'],
+          ['<span style="color:#e690d1;">968699969</span>', '行9列2', '23424.90'],
+        ],
+        index: true,
+        columnWidth: [50],
+        align: ['center']
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.box{
+  width: 97%;
+  height: 97%;
+}
+</style>
