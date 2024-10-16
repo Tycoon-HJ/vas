4c6afd845c0cf39389b836f159e58a92bd80c5a5
yinhaijun <2523284817@qq.com>
2024-10-16 23:53:52 +0800

    add rollup plugin

---
+++ vas-datav-libs-dev/rollup.config.dev.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const resolve = require('rollup-plugin-node-resolve')
+
+
+const inputPath = path.resolve(__dirname, './src/index.js')
+const outputUmdPath = path.resolve(__dirname, './dist/imooc.datav.js')
+const outputEsPath = path.resolve(__dirname, './dist/imooc.datav.es.js')
+
+module.exports = {
+    input: inputPath,
+    output: [{
+        file: outputUmdPath, format: 'umd', name: 'imoocDatav'
+    }, {
+        file: outputEsPath, format: 'es', name: 'imoocDatav'
+    }],
+    plugins: [
+        resolve()
+    ]
+}
---
+++ vas-datav-libs-dev/src/index.js
@@ -0,0 +1,10 @@
+// const data = require("sam-test-data");
+// console.log(data.random(), data.a, data.b);
+//
+// module.exports = {}
+
+// es6
+import * as  data from 'sam-test-data'
+console.log(data.random(), data.a, data.b);
+
+export default  {}
