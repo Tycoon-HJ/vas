f2b6ae2b51b4c08b7e84fef62ceac8f0d2c18a6a
yinhaijun <2523284817@qq.com>
2024-10-19 13:12:51 +0800

    在html页面中添加按钮

--- vas-datav-libs-dev/src/Test.vue
+++ vas-datav-libs-dev/src/Test.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="test">
-    {{ message }}
+
+
+
+
+
+    <div>
+      message: {{ message }}
+    </div>
+    <div>
+      doubleCount: {{ doubleCount }}
+    </div>
+    <button @click="add"> add count</button>
+
   </div>
 </template>
 <script>
+import {computed, ref} from 'vue'
+
 export default {
   name: 'TestComponent',
   setup() {
     const message = 'hello'
+    const count = ref(1)
+    const doubleCount = computed(() => count.value * 2)
+    const add = () => {
+      count.value++
+    }
     return {
-      message
+      message,
+      doubleCount,
+      add
     }
   }
 }
