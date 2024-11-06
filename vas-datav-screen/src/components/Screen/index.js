1cf17b52c7ae8e0e59a0ecc2a39b73c95a3400db
yinhaijun <2523284817@qq.com>
2024-11-06 20:57:42 +0800

    新项目引入组件成功！！！！！

--- vas-datav-screen/src/components/Screen/index.js
+++ vas-datav-screen/src/components/Screen/index.js
@@ -1,4 +1,4 @@
-import Screen from './Screen.vue';
-export default function (Vue){
+ import Screen from './Screen.vue';
+export default function (Vue) {
     Vue.component(Screen.name, Screen);
 }
--- vas-datav-screen/src/main.js
+++ vas-datav-screen/src/main.js
@@ -1,9 +1,6 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import Screen from './components/Screen/index.js'
-createApp(App)
-    .use(ElementPlus)
-    .use(Screen).mount('#app')
+import  Container from './lib/imooc.datav.es.js'
+import Screen from "@/components/Screen/index.js";
+createApp(App).use(Container).use(Screen).mount('#app')
