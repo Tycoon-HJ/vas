7fd8ae7715577b2c2a74196cc6b962f1a4eae703
yinhaijun <2523284817@qq.com>
2024-11-07 21:33:07 +0800

    删除多余代码

--- imooc-datav-report-dev/src/mixins/commonCardMixin.js
+++
@@ -1,7 +0,0 @@
-import CommonCard from '../components/CommonCard/index.vue'
-
-export default {
-  components: {
-    CommonCard
-  }
-}
--- vas-datav-vue3/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-import './assets/imooc.datav.js'
-import {createApp} from 'vue'
-import App from './App.vue'
-
-
-createApp(App).use(imoocDatav,{})
-    .mount('#app')
